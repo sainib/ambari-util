d0f9435627f284e6a28ff2202e075f3b95fd47d5
sainib <bsaini@hortonworks.com>
2015-09-22 23:22:30 -0400

    updated preinstall check

--- preInstallCheck/precheck.sh
+++ preInstallCheck/precheck.sh
@@ -3,57 +3,66 @@
 
 wait_time=10
 
-echo "Checking Mount"
 echo "---------------------------------------------"
+echo ">>>>> Checking Mount"
 echo "sudo mount 2>/dev/null | grep -v \#"
 sudo mount 2>/dev/null | grep -v \#
 echo ""
 echo ""
+echo "---------------------------------------------"
+
+
+echo "---------------------------------------------"
+echo ">>>>> Checking UMASK"
+echo "umask"
+umask
+echo ""
+echo ""
+echo "---------------------------------------------"
 
 sleep ${wait_time}
 
-echo "Checking limits.conf"
-echo "---------------------------------------------"
+echo ">>>>> Checking limits.conf"
 echo "sudo cat /etc/security/limits.conf  2>/dev/null | grep -v \#"
 sudo cat /etc/security/limits.conf  2>/dev/null | grep -v \#
 echo ""
 echo ""
+echo "---------------------------------------------"
 
 sleep ${wait_time}
 
 
-echo "Checking sysctl.conf"
-echo "---------------------------------------------"
+echo ">>>>> Checking sysctl.conf"
 echo "sudo cat /etc/sysctl.conf  2>/dev/null | grep -v \#"
 sudo cat /etc/sysctl.conf  2>/dev/null | grep -v \#
 echo ""
 echo ""
+echo "---------------------------------------------"
 
 sleep ${wait_time}
 
-echo "Checking Services"
-echo "---------------------------------------------"
+echo ">>>>> Checking Services"
 echo "sudo chkconfig --list  2>/dev/null | grep ntp  "
 sudo chkconfig --list  2>/dev/null | grep ntp 
 echo "sudo chkconfig --list  2>/dev/null | grep iptable "
 sudo chkconfig --list  2>/dev/null | grep iptable
 echo ""
 echo ""
+echo "---------------------------------------------"
 
 sleep ${wait_time}
 
 
-echo "Checking rc.local"
-echo "---------------------------------------------"
+echo ">>>>> Checking rc.local"
 echo "sudo cat /etc/rc.local 2>/dev/null | grep -v \#"
 sudo cat /etc/rc.local 2>/dev/null | grep -v \#
 echo ""
 echo ""
+echo "---------------------------------------------"
 
 sleep ${wait_time}
 
 echo "Core HDP / Hadoop Users"
-echo "---------------------------------------------"
 echo ""
 echo "HDP `grep -c HDP /etc/passwd`"
 echo "oozie `grep -c oozie /etc/passwd`"
@@ -83,12 +92,12 @@ echo "ranger `grep -c ranger /etc/passwd`"
 echo "xapolicymgr `grep -c xapolicymgr /etc/passwd`"
 echo ""
 echo ""
+echo "---------------------------------------------"
 
 sleep ${wait_time}
 
 
-echo "Checking HDP / Hadoop Supporting Users"
-echo "---------------------------------------------"
+echo ">>>>> Checking HDP / Hadoop Supporting Users"
 echo ""
 
 echo "postgres `grep -c postgres /etc/passwd`"
@@ -98,53 +107,51 @@ echo "nfsnobody `grep -c nfsnobody /etc/passwd`"
 
 echo ""
 echo ""
-
+echo "---------------------------------------------"
 sleep ${wait_time}
 
 
-echo "Checking Commands"
-echo "---------------------------------------------"
+echo ">>>>> Checking Commands"
 echo "command -v hdfs"
 command -v "hdfs" 
 echo "command -v hadoop"
 command -v "hadoop"
 echo ""
 echo ""
-
+echo "---------------------------------------------"
 sleep ${wait_time}
 
 
-echo "Checking background processes for resource consumptions"
-echo "---------------------------------------------"
+echo ">>>>> Checking background processes for resource consumptions"
 echo "top -n 1 -b -o +%CPU | head -20"
 top -n 1 -b -o +%CPU | head -20
-echo "---------------------------------------------"
+echo ""
 echo "top -n 1 -b -o +%MEM | head -20"
 top -n 1 -b -o +%MEM | head -20
 echo ""
+echo "---------------------------------------------"
 
 sleep ${wait_time}
 
-
-echo "Checking Libraries"
-echo "---------------------------------------------"
+echo ">>>>> Checking Libraries"
 echo "rpm -qa | grep mapr "
 rpm -qa | grep mapr 
-echo "---------------------------------------------"
+echo ""
 echo "ls -ltra /opt/mapr"
 ls -ltra /opt/mapr
-echo "---------------------------------------------"
+echo ""
 echo "ls -ltra /opt/cores"
 ls -ltra /opt/cores
-echo "---------------------------------------------"
+echo ""
 echo "ls -ltra /usr"
 ls -ltra /usr
-echo "---------------------------------------------"
 echo ""
+echo ""
+echo "---------------------------------------------"
 
 sleep ${wait_time}
 
 
-echo "Checking Done"
+echo ">>>>> Checking Done"
 echo "---------------------------------------------"
 echo ""
