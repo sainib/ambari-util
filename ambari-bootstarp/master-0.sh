cc57710bf966deacec765c97b5545ef88a278abb
sainib <bsaini@hortonworks.com>
2017-04-04 08:30:13 -0400

    renamed ambari-bootstrap

---
+++ ambari-bootstarp/master-0.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+yum install -y -q curl ntp openssl python zlib wget unzip openssh-clients
+echo ""
+echo "1 - Creating SSH Key on Master"
+
+ssh-keygen
+
+cd ~/.ssh
+cat id_rsa.pub >> authorized_keys
+
+cd ~/.ssh
+
+echo ""
+echo ""
+cat id_rsa.pub
+echo ""
+echo ""
