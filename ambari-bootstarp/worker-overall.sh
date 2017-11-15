a2c6c2abb8bb136c3a9ced125f973edbf553d4fd
BIRENDER SAINI <birender.saini@gmail.com>
2017-11-14 22:01:31 -0500

    Update worker-overall.sh

--- ambari-bootstarp/worker-overall.sh
+++ ambari-bootstarp/worker-overall.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 
 bash run_command.sh 'ifconfig'  | grep broadcast | grep netmask
+bash run_command.sh 'yum -y update'
 bash run_command.sh 'yum install -y -q curl ntp openssl python zlib wget unzip openssh-clients'
 bash run_command.sh 'systemctl is-enabled ntpd'
 bash run_command.sh 'systemctl enable ntpd'
