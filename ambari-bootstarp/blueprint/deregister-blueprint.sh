e5977866f9807d870db17d263df3129cee7eea49
root <root@birens0.field.hortonworks.com>
2018-02-21 15:26:08 +0000

    blueprint deploy

--- ambari-bootstarp/blueprint/deregister-blueprint.sh
+++ ambari-bootstarp/blueprint/deregister-blueprint.sh
@@ -1,1 +1,1 @@
-curl -H "X-Requested-By: ambari" -u admin:admin -i -X DELETE  http://birens0.field.hortonworks.com:8080/api/v1/blueprints/openstack_8node_cluster
+curl -H "X-Requested-By: ambari" -u admin:admin -i -X DELETE  http://birens0.field.hortonworks.com:8080/api/v1/blueprints/secloud-openstack-8node
--- ambari-bootstarp/blueprint/register-blueprint.sh
+++ ambari-bootstarp/blueprint/register-blueprint.sh
@@ -1,1 +1,17 @@
-curl -H "X-Requested-By: ambari" -u admin:admin -X POST -d @8node-blueprint.json http://birens0.field.hortonworks.com:8080/api/v1/blueprints/openstack_8node_cluster
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "ERROR: Profile the file name to store the blueprint"
+	exit 1
+fi
+fn=$1
+if [  -f "$fn" ] ; then
+                echo "ERROR: File already exists. We dont want to overwrite the existing file, do we?"
+                exit 1
+fi 
+
+rm active_bp.json
+cp $fn active_bp.json
+
+curl -H "X-Requested-By: ambari" -u admin:admin -X POST -d @active_bp.json http://birens0.field.hortonworks.com:8080/api/v1/blueprints/secloud-openstack-8node
+
