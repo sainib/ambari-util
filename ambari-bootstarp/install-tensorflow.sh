af6b02cb6de7638ea5efbc8d567ee3b4b269ded5
BIRENDER SAINI <birender.saini@gmail.com>
2018-05-04 11:08:09 -0400

    Update install-tensorflow.sh

--- ambari-bootstarp/install-tensorflow.sh
+++ ambari-bootstarp/install-tensorflow.sh
@@ -1,3 +1,5 @@
+* INSTALLATION 
+------------------------ 
 sudo yum -y install epel-release
 sudo yum -y install gcc gcc-c++ python-pip python-devel atlas atlas-devel gcc-gfortran openssl-devel libffi-devel
 # use pip or pip3 as you prefer for python or python3
@@ -7,3 +9,9 @@ chmod -R 755 /opt/tensorflow
 source /opt/tensorflow/bin/activate
 pip install --upgrade numpy scipy wheel cryptography #optional
 pip install --upgrade tensorflow
+
+
+
+* USAGE
+------------------------
+source /opt/tensorflow/bin/activate
